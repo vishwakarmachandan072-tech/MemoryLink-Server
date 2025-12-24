cef0c02e5796a6928405c56df4a96c8688537549
vishwakarmachandan072-tech <vishwakarmachandan072@gmail.com>
2025-12-24 14:07:08 +0530

    Update userRouter to handle username and email changes

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,10 +3,12 @@ import express from 'express';
 
 //Local module
 import protectedRoute from '../middleware/auth.middleware.js';
-import{ validateUsername } from '../controllers/userController.js';
+import{ changeEmail,  changeUsername,  validateUsername } from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.patch('/profile/:id', protectedRoute, validateUsername);
+router.patch('/profile/usernameValidate/:id', protectedRoute, validateUsername);
+router.patch('/profile/changeUsername/:id', protectedRoute, changeUsername);
+router.patch('/profile/changeEmail/:id', protectedRoute, changeEmail)
 
 export default router;
